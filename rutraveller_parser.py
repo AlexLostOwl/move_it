79248362b21ad14707d5a8988ef252e89e5ff73e
sid8l <milosida@gmail.com>
2019-10-06 15:41:54 +0300

    remove page generator, add verified check and location

--- rutraveller_parser.py
+++ rutraveller_parser.py
@@ -9,35 +9,42 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='rutravveler_parse.log')
 
 
-def get_html(url, page_num=0):
-    while True:
-        page_num += 1
-        try:
-            page = requests.get(f'{url}/{page_num}')
-            page.raise_for_status()
-            errors_count = 0
-            yield page.text
-        except requests.RequestException:
-            logging.info('404 found at %s/%d', url, page_num)
-            if errors_count == 3:
-                logging.INFO('Pages with 404 error were %d in a row. Stop parsing', page_num)
-                break
-            else:
-                errors_count += 1
-                continue
+def get_html(url, page_num):
+    try:
+        page = requests.get(f'{url}/{page_num}')
+        page.raise_for_status()
+        return page.text
+    except requests.RequestException:
+        logging.info('404 found at %s/%d', url, page_num)
+        return False
 
 
-def get_places(url):
+def get_places(url, page_num=1):
     logging.info('Parsing started')
-    for page_html in get_html(url, page_num=5):
+    errors_count = 0
+    while True:
+        page_html = get_html(url, page_num)
+        page_num += 1
+        if errors_count == 4 and not page_html:
+            logging.info('Pages with 404 error were %d in a row. Stop parsing', errors_count + 1)
+            break
+        if not page_html:
+            errors_count += 1
+            continue
+        errors_count = 0
         soup = BeautifulSoup(page_html, 'html.parser')
+        if soup.find('span', class_='label-status--notverified'):
+            logging.info('page %s/%d not verified, skipping', url, page_num)
+            continue
         name = soup.find('h1', class_='title-h1').text
         description = soup.find('div', class_='place-description').find('div', class_='text').text
+        location = soup.find('span', class_='info-line__text_gray').text
         place_info = {
             'place_name': name,
-            'place_description': description
+            'place_description': description,
+            'location': location
         }
-        print(f'{place_info["place_name"]}')
+        print(f'{place_info["location"]}')
 
 
 if __name__ == "__main__":
