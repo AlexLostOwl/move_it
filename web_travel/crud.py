b71558bc0e74662d0f196c969998ab1ff4694be6
Novikov Artem <milosida@gmail.com>
2019-10-15 08:30:01 +0000

    added a check for country when check place and city

--- web_travel/crud.py
+++ web_travel/crud.py
@@ -30,7 +30,7 @@ def city_exists(city_name, related_country):
     same_cities_objects = City.query.filter(City.city_name == city_name).all()
     for city_object in same_cities_objects:
         country_object = Country.query.filter(Country.id == city_object.country_id)
-        if country_object.first().country_name == related_country:
+        if country_object.count() and country_object.first().country_name == related_country:
             return True
     return False
 
@@ -55,7 +55,7 @@ def place_exists(place_name, related_country):
     same_places_objects = Place.query.filter(Place.place_name == place_name).all()
     for place_object in same_places_objects:
         country_object = Country.query.filter(Country.id == place_object.country_id)
-        if country_object.first().country_name == related_country:
+        if country_object.count() and country_object.first().country_name == related_country:
             return True
     return False
 
