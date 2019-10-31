d90eeaae575b38a4f6d5c731ec5c35b5928dcd2e
sid8l <milosida@gmail.com>
2019-10-31 22:18:32 +0300

    fix js

--- web_travel/static/get-cities.js
+++ web_travel/static/get-cities.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
-    console.log(1)
-    if ($(this).val() != 'default') {
-        console.log(2)
-        var Country = $(this).find('option:selected').text();
 
+    const getAjax = (country) => {
         $.ajax({
             type: 'GET',
             url: "../country/getCities",
-            data: { country: Country},
+            data: { country: country},
             contentType: 'application/json',
             dataType: 'json',
             success: function(response) {
-                console.log(response)
                 let options = [];
                 const first_option = '<option value="" selected>Выберите из списка</option>'
                 options.push(first_option)
@@ -35,41 +31,19 @@ $(document).ready(function(){
         });
     }
 
+    if ($(this).val() != 'default') {
+        var country = $(this).find('option:selected').text();
+
+        getAjax(country)
+    }
+
     $("select#countriesSelect").change(function() {
 
         if ($(this).val() != 'default') {
 
-            var Country = $(this).find('option:selected').text();
+            var country = $(this).find('option:selected').text();
 
-            $.ajax({
-                type: 'GET',
-                url: "../country/getCities",
-                data: { country: Country},
-                contentType: 'application/json',
-                dataType: 'json',
-                success: function(response) {
-                    console.log(response)
-                    let options = [];
-                    const first_option = '<option value="" selected>Выберите из списка</option>'
-                    options.push(first_option)
-                    if (response.cities.length == 0){
-                        $('select#citiesSelect').html(first_option);
-                        $('#groupCityInput').hide();
-                    }
-
-                    else{
-                        for (var i=0, l=response.cities.length; i<l; i++){
-                            options.push('<option>'+response.cities[i]+'</option>');
-                        }
-                        
-                        
-                        $('select#citiesSelect').html(options.join(''));
-
-                        $('#groupCityInput').show();
-                    }
-
-                }
-            });
+            getAjax(country);
         }
         else { 
             const first_option = '<option value="" disabled selected>Выберите из списка</option>'
