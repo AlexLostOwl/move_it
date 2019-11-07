7efd96d5601f83b6e138c01f19172f0a8bc821ee
Alexander Plotnikov <alexlostowl@gmail.com>
2019-11-07 04:26:55 +0300

    Add photos to db and create index page with carousel

--- web_travel/main/views.py
+++ web_travel/main/views.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, render_template
 
+from web_travel.crud import get_random_place, get_description_by_place, get_photo_by_place
+
 blueprint = Blueprint('main', __name__)
 
 
 @blueprint.route('/')
 def index():
-    title = "Travel Project"
-    test_text = "Hello this is the test page"
-    return render_template('main/index.html', page_title=title, text=test_text)
+    welcome_string = "Travel Project"
+    carousel_items = []
+    for i in range(3):
+        items = {'place': get_random_place()}
+        description = get_description_by_place(items['place'])
+        description = description[:100] + '..' if len(description) > 100 else description
+        items['description'] = description
+        items['photo_link'] = get_photo_by_place(items['place'])
+        carousel_items.append(items)
+    return render_template('main/index.html', jumbo_head=welcome_string, carousel_items=carousel_items)
--- web_travel/place/models.py
+++ web_travel/place/models.py
@@ -9,5 +9,17 @@ class Place(db.Model):
     country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
     city_id = db.Column(db.Integer, db.ForeignKey('cities.id'))
 
+    photos = db.relationship('Photo', backref='photo_in_place', lazy='dynamic')
+
     def __repr__(self):
         return f'<Place {self.place_name} id={self.id}>'
+
+
+class Photo(db.Model):
+    __tablename__ = 'photos'
+    id = db.Column(db.Integer, primary_key=True)
+    photo_link = db.Column(db.String)
+    place_id = db.Column(db.Integer, db.ForeignKey('places.id'))
+
+    def __repr__(self):
+        return f'<Photo {self.photo_link} id={self.id}>'
